1f69a8860b8cff3f8399f9aa5f2f7169c031c7b5
Ben Hirsch <BenHirsch@lawn-128-61-60-163.lawn.gatech.edu>
2014-02-07 11:18:39 -0500

    Set up login system and reformatted xkcd

--- PersonalHandler.py
+++ PersonalHandler.py
@@ -3,10 +3,11 @@ import jinja2
 import urllib2
 import xml.dom.minidom
 import logging
+import string
 
-def login():
-	user = "benhirsch42"
-	passwd = "diagonalelectrodiligentpantry"
+def login(username, password):
+	user = username
+	passwd = password
 	auth_handler = urllib2.HTTPBasicAuthHandler()
 	auth_handler.add_password(
 		realm='New mail feed',
@@ -33,15 +34,19 @@ def get_xkcd_data():
 def get_gmail_tsquare_data():
 	gmail_tsquare_url = "https://mail.google.com/mail/feed/atom/t-square"
 	gmail_tsquare_content = urllib2.urlopen(gmail_tsquare_url).read()
+	gmail_tsquare_content = gmail_tsquare_content.replace("""<link rel="alternate" href=\"""", "<link>")
+	gmail_tsquare_content = gmail_tsquare_content.replace("""\" type="text/html" />""", "</link>")
 	dom = xml.dom.minidom.parseString(gmail_tsquare_content)
 	dom_entries = dom.getElementsByTagName("entry")
 	titles = []
 	summaries = []
-	# links = []
+	links = []
 	for i in range(len(dom_entries)):
 		titles.append(dom_entries[i].getElementsByTagName("title")[0].childNodes[0].data)
 		summaries.append(dom_entries[i].getElementsByTagName("summary")[0].childNodes[0].data)
-	return titles, summaries
+		link = dom_entries[i].getElementsByTagName("link")[0].childNodes[0].data
+		links.append(link)
+	return titles, summaries, links
 
 def get_show_data():
 	show_url = "http://showrss.info/rss.php?user_id=164896&hd=null&proper=null"
@@ -63,12 +68,24 @@ def get_show_data():
 
 class PersonalHandler(Handler):
 	def get(self):
-		login()
+		logged_in = False
+		xkcd = get_xkcd_data()
+		show_data = get_show_data()
+		multi_tab = """window.open("https://mail.google.com/mail/u/0/?shva=1#inbox", name="_blank").focus(); window.open("http://www.youtube.com/feed/subscriptions", name="_blank").focus(); window.open("http://www.reddit.com/", name="_blank").focus();"""
+		self.render("personal.html", multi_tab = multi_tab, xkcd_title = xkcd[0], xkcd_img = xkcd[1],
+			show_titles = show_data[0], show_links = show_data[1], show_times = show_data[2], logged_in = logged_in)
+
+	def post(self):
+		username = self.request.get("username")
+		password = self.request.get("password")
+		login(username, password)
+		logged_in = True
 		xkcd = get_xkcd_data()
 		gmail_tsquare = get_gmail_tsquare_data()
 		show_data = get_show_data()
 		multi_tab = """window.open("https://mail.google.com/mail/u/0/?shva=1#inbox", name="_blank").focus(); window.open("http://www.youtube.com/feed/subscriptions", name="_blank").focus(); window.open("http://www.reddit.com/", name="_blank").focus();"""
 		self.render("personal.html", multi_tab = multi_tab, xkcd_title = xkcd[0], xkcd_img = xkcd[1],
-			tsquare_titles = gmail_tsquare[0], tsquare_summaries = gmail_tsquare[1], show_titles = show_data[0],
-			show_links = show_data[1], show_times = show_data[2])
+			tsquare_titles = gmail_tsquare[0], tsquare_summaries = gmail_tsquare[1], tsquare_links = gmail_tsquare[2],
+			show_titles = show_data[0], show_links = show_data[1], show_times = show_data[2], logged_in = logged_in)
 
+	
